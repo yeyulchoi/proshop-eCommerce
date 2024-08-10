7309c12ffb19d1744c4782a7543919a3c79d7c18
yeyulchoi <yeyulchoi@outlook.com>
2024-08-10 02:46:30 -0400

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    <div>
+      <h1>hello proshop</h1>
+      <Header />
+    </div>
+  )
+}
+
+export default App
